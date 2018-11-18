6f69788b7d7c2ad38bb54dccf3191c63fccaaf84
Cyril Yared <32401419+cyrilyared@users.noreply.github.com>
2018-11-18 14:44:13 -0500

    Update server.js

--- server.js
+++ server.js
@@ -5,13 +5,21 @@ var history = require('connect-history-api-fallback');
 var app = express();
 app.use(serveStatic(path.join(__dirname, 'dist')));
 
-
 history({
   index: '/',
   rewrites: [{from: /\/login/, to: '/'}],
 });
 
-app.use(history());
+app.use(history({
+  disableDotRule: true,
+  verbose: true
+}));
+
+app.use(serveStatic(path.join(__dirname, 'dist')));
+
+app.get('/', function (req, res) {
+  res.render(path.join(__dirname + '/dist/index.html'));
+});
 
 const port = process.env.PORT || 80;
 app.listen(port);
