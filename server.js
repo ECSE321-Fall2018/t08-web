24b067f51c70286250a2a80e064abdc85c2bd09e
projeffboy <jeffery124@gmail.com>
2018-11-23 22:39:23 -0500

    Cleaned up server.js

--- server.js
+++ server.js
@@ -1,61 +1,25 @@
-var express = require('express');
-const serveStatic = require("serve-static");
-const path = require('path');
-var history = require('connect-history-api-fallback');
-var app = express();
-app.use(serveStatic(path.join(__dirname, 'dist')));
+let express = require('express')
+const serveStatic = require('serve-static')
+const path = require('path')
+let history = require('connect-history-api-fallback')
+let app = express()
+app.use(serveStatic(path.join(__dirname, 'dist')))
 
 history({
   index: '/',
   rewrites: [{from: /\/login/, to: '/'}],
-});
+})
 
 app.use(history({
   disableDotRule: true,
   verbose: true
-}));
+}))
 
-app.use(serveStatic(path.join(__dirname, 'dist')));
+app.use(serveStatic(path.join(__dirname, 'dist')))
 
 app.get('/', function (req, res) {
-  res.render(path.join(__dirname + '/dist/index.html'));
-});
+  res.render(path.join(__dirname + '/dist/index.html'))
+})
 
-const port = process.env.PORT || 80;
-app.listen(port);
-
-/* let connect = require('connect');
-
-let app = connect()
-  .use(history())
-  .listen(3000); */
-
-
-// let app = express();
-
-
-  
-/*   history({
-    rewrites: [
-      {
-        from: /^\/libs\/.*$/,
-        to: function(context) {
-          return '/bower_components' + context.parsedUrl.pathname;
-        }
-      }
-    ]
-  }); */
-
-  /* history({
-    verbose: true
-  });
-  
-  history({
-    htmlAcceptHeaders: ['text/html', 'application/xhtml+xml']
-  });
-
-  history({
-    disableDotRule: true
-  });
- */
-  
+const port = process.env.PORT || 80
+app.listen(port)
